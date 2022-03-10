37502b8f50debbca8bedfa0378a2ae8b144fb7d8
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-10 18:59:49 -0300

    add cors para as rotas get de acesso api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const vinhosController = require("../controllers/vinhosController");
 const routes = express.Router();
+const cors = require("cors");
 
-routes.get("/vinhos/lista", vinhosController.listarVinhos);
+routes.get("/vinhos/lista", cors(), vinhosController.listarVinhos);
 routes.post("/vinhos/criar", vinhosController.cadastrarVinhos);
 routes.delete("/vinhos/:id/deletar", vinhosController.deletarVinho);
 routes.put("/vinhos/:id/atualizar", vinhosController.atualizarVinho);
 
-routes.get("/vinhos/tintos", vinhosController.listarVinhosTintos);
-routes.get("/vinhos/brancos", vinhosController.listarVinhosBrancos);
-routes.get("/vinhos/roses", vinhosController.listarVinhosRoses);
-routes.get("/vinhos/espumantes", vinhosController.listarVinhosEspumantes);
+routes.get("/vinhos/tintos", cors(), vinhosController.listarVinhosTintos);
+routes.get("/vinhos/brancos", cors(), vinhosController.listarVinhosBrancos);
+routes.get("/vinhos/roses", cors(), vinhosController.listarVinhosRoses);
+routes.get(
+  "/vinhos/espumantes",
+  cors(),
+  vinhosController.listarVinhosEspumantes
+);
 
 module.exports = routes;
