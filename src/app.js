3a1ee379fdb5eafd8e83731f4247f6e66fd5a333
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-04 20:17:39 -0300

    teste api

--- src/app.js
+++ src/app.js
@@ -9,15 +9,15 @@ const app = express();
 
 db.hasConection();
 
-app.use(express.json());
+//app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Headers", "*");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET");
 
-  next();
-});
+//   next();
+// });
 
 app.use(routes);
 
