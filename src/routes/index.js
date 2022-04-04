6872479c9b1594988b8bf4cb39c52bf895448e06
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-04 19:22:39 -0300

    cors removido por rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,16 @@ const pratosController = require("../controllers/pratosController");
 const routes = express.Router();
 const cors = require("cors");
 
-routes.get("/vinhos/lista", cors(), vinhosController.listarVinhos);
+routes.get("/vinhos/lista", vinhosController.listarVinhos);
 routes.post("/vinhos/criar", vinhosController.cadastrarVinhos);
 routes.delete("/vinhos/:id/deletar", vinhosController.deletarVinho);
 routes.put("/vinhos/:id/atualizar", vinhosController.atualizarVinho);
 
-routes.get("/vinhos/tintos", cors(), vinhosController.listarVinhosTintos);
-routes.get("/vinhos/brancos", cors(), vinhosController.listarVinhosBrancos);
-routes.get("/vinhos/roses", cors(), vinhosController.listarVinhosRoses);
-routes.get(
-  "/vinhos/espumantes",
-  cors(),
-  vinhosController.listarVinhosEspumantes
-);
-routes.get("/vinhos/:id", cors(), vinhosController.listarVinho);
-routes.get("/harmonizacao/:id", cors(), pratosController.harmonizacoes);
+routes.get("/vinhos/tintos", vinhosController.listarVinhosTintos);
+routes.get("/vinhos/brancos", vinhosController.listarVinhosBrancos);
+routes.get("/vinhos/roses", vinhosController.listarVinhosRoses);
+routes.get("/vinhos/espumantes", vinhosController.listarVinhosEspumantes);
+routes.get("/vinhos/:id", vinhosController.listarVinho);
+routes.get("/harmonizacao/:id", pratosController.harmonizacoes);
 
 module.exports = routes;
