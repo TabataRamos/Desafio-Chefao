5fe2623770650c42921b46c268a9b2be25d2e40a
TabataRamos <tabata.b.ramos@gmail.com>
2022-04-04 17:05:40 -0300

    Controle de Acesso API

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ db.hasConection();
 app.use(express.json());
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET");
   app.use(cors());
