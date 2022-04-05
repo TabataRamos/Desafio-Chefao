a69bb1e22dd2766f59774b6b64faa2405f93b108
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-05 15:28:34 -0300

    método atualizado

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ const app = express();
 db.hasConection();
 
 app.use(express.json());
+app.use(cors());
+// app.use((req, res, next) => {
+//   // res.header("Access-Control-Allow-Headers", "*");
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   // res.header("Access-Control-Allow-Methods", "GET");
 
-app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Headers", "*");
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Methods", "GET");
-  app.use(cors());
-  next();
-});
+//   next();
+// });
 
 app.use(routes);
 
